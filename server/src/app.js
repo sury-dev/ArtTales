d6d9103c22056fc493ecf400b4d634e0547d7370
Suryansh Pandey <coderhood0011@gmail.com>
2024-11-20 20:11:02 +0530

    Backend origin port update

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 // }))
 
 app.use(cors({
-    origin: ["https://art-tales-frontend.vercel.app/", "http://localhost:3000"],
+    origin: ["https://arttales-1.onrender.com/", "http://localhost:3000"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
     }
